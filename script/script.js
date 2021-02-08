b510c5768bd457942a0f6b2db572db6742fcc723
Sergey Pichugov <polarizaciacd@gmail.com>
2021-02-08 11:39:21 +0300

    random array number

--- script/script.js
+++ script/script.js
@@ -1,108 +1,37 @@
 'use strict';
 
+function generateOddNumber(x, y) {
+   let min;
+   let max;
 
-function randomGame() {
+   if (x < y) {
+      min = x;
+      max = y;
+   } else {
+      min = y;
+      max = x;
+   }
 
-   const isNumber = function (n) {
-      return !isNaN(parseFloat(n)) && isFinite(n);
-   };
-   
-   const testNum = function(x) {
-      if (isNumber(x) || x > 100 || x < 1) {
-         return true;
-      } else {
-         return false;
+   let arr = [];
+   let arrOdd = [];
+   let tmp = max - min;
+
+   for (let i = 0; i <= tmp; i++){
+      arr[i] = min;
+      min++;
+   }
+
+   for (let i = 0; i < arr.length; i++) {
+      if ( arr[i] % 2 !== 0) {
+         arrOdd.push(arr[i]);
       }
-   };
-   
-
-   let attempts = 10;
-
-   
-   const start = function() {
-      
-      
-      const counter = function () {
-         attempts--;
-         console.log(attempts);
-         if (attempts === 0) {
-            let message = confirm('Попытки закончились, хотите сыграть еще ?');
-            if (message === true) {
-               attempts = 10;
-               return start();
-            } else {
-               alert('Игра окончена');
-            }
-         } else {
-            return sortNumber(tmp);
-   
-         }
-      };
-      
-
-
-            
-      const mysteryNum = Math.floor((Math.random() * 100) + 1);
-      console.log(mysteryNum);
-   
-      let myNumber = prompt('Угадай число от 1 до 100 ');
-      if (myNumber === null) {
-         alert('Игра окончена');
-         return;
-      }
-      
-
-      let tmp = myNumber;
-
-      const sortNumber = function() {
-         console.log(tmp);
-         
-         if (!testNum(tmp) || tmp === '' ) {
-            console.log(tmp);
-            tmp = prompt('Введи число!');
-            sortNumber(tmp);
-            return;
-         }
-
-         if (tmp === null) {
-            console.log('вышел');
-            alert('Игра окончена');
-            return;
-         }
-
-         if (tmp < mysteryNum) {
-            tmp = prompt('Загаданное число больше, осталось попыток  ' + attempts);
-            counter();
-
-         } else if (tmp > mysteryNum) {
-            tmp = prompt('Загаданное число меньше, осталось попыток  ' + attempts);
-            counter();
-         } else {
-            console.log('угадал');
-            let message = confirm('Поздравляю, Вы угадали! Хотели бы сыграть еще?');
-            if (message === true) {
-               attempts = 10;
-               return start();
-            }
-         }
-
-
-
-
-         
-      };
-
-      sortNumber();
-
-
-   };
-
-   start();
-
-
-   
-
-
+   }
+   return arrOdd[Math.floor(Math.random() * arrOdd.length)];
 }
 
-randomGame();
+
+console.log(generateOddNumber(1, 100));
+console.log(generateOddNumber(0, -10));
+console.log(generateOddNumber(-7, -3));
+console.log(generateOddNumber(-100, 100));
+console.log(generateOddNumber(1, -1));
